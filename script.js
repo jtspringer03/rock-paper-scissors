71cbfa8465572050cbc7ce9c42f32609fc2b1137
jtspringer03 <jtspringer03@yahoo.com>
2022-12-20 12:13:25 +0800

    complete playround function

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ function getPlayerChoice() {
     }
   }
 }
+
+function checkWinner(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "tie";
+  } else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    return "player";
+  } else {
+    return "computer";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = getPlayerChoice();
+  computerSelection = getComputerChoice();
+  const result = checkWinner(playerSelection, computerSelection);
+  if (result == "tie") {
+    console.log("It was a tie!");
+  } else if (result == "player") {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+  } else {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`);
+  }
+}
