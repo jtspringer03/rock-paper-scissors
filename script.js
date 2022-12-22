b44c4eddf0e65620d9bc27a4ffedefbd51d54946
jtspringer03 <jtspringer03@yahoo.com>
2022-12-22 14:51:17 +0800

    complete ui score keeping

--- script.js
+++ script.js
@@ -5,66 +5,54 @@ function getComputerChoice() {
   return selection;
 }
 
-function getPlayerChoice() {
-  let validatedInput = false;
-  while (validatedInput == false) {
-    const userInput = prompt("rock, paper, or scissors?");
-    if (userInput == null) {
-      continue;
-    }
-    const userInputInLower = userInput.toLowerCase();
-    if (options.includes(userInputInLower)) {
-      validatedInput = true;
-      return userInputInLower;
-    }
-  }
-}
-
 function checkWinner(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     return "tie";
-  } else if (
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper")
-  ) {
+  } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
     return "player";
   } else {
     return "computer";
   }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
   const results = checkWinner(playerSelection, computerSelection);
   if (results == "tie") {
     return "It's a tie!";
   } else if (results == "player") {
+    playerScore++;
     return `You win! ${playerSelection} beats ${computerSelection}!`;
   } else {
+    computerScore++;
     return `You Lose! ${computerSelection} beats ${playerSelection}!`;
   }
 }
 
-function game() {
-  let scorePlayer = 0;
-  let scoreComputer = 0;
-  for (let i = 0; i < 5; i++) {
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    if (checkWinner(playerSelection, computerSelection) == "player") {
-      scorePlayer++;
-    } else if (checkWinner(playerSelection, computerSelection)) {
-      scoreComputer++;
-    }
-  }
-  if (scorePlayer > scoreComputer) {
-    console.log("Player is the Winner!");
-  } else if (scorePlayer < scoreComputer) {
-    console.log("Computer is the Winner!");
-  } else {
-    console.log("It was a tie!");
-  }
-}
+// DOM Manipulation
 
-game();
+// Borders
+document.querySelector("#results1").style.borderStyle = "solid";
+document.querySelector("#player-score").style.borderStyle = "solid";
+document.querySelector("#computer-score").style.borderStyle = "solid";
+
+// Button Clicks
+document.querySelector("#button-rock").addEventListener("click", function () {
+  document.getElementById("results1").textContent = playRound("rock", getComputerChoice());
+  document.getElementById("player-score").textContent = playerScore;
+  document.getElementById("computer-score").textContent = computerScore;
+});
+
+document.querySelector("#button-paper").addEventListener("click", function () {
+  document.getElementById("results1").textContent = playRound("paper", getComputerChoice());
+  document.getElementById("player-score").textContent = playerScore;
+  document.getElementById("computer-score").textContent = computerScore;
+});
+
+document.querySelector("#button-scissors").addEventListener("click", function () {
+  document.getElementById("results1").textContent = playRound("scissors", getComputerChoice());
+  document.getElementById("player-score").textContent = playerScore;
+  document.getElementById("computer-score").textContent = computerScore;
+});
